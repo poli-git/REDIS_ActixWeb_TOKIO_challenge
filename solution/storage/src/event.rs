7966822fccb11c985d5214d1be1b1e3eab3f0e62
Pablo <pbisceglia@gmail.com>
2025-05-31 21:26:05 -0300

    Update event.rs

--- solution/storage/src/event.rs
+++ solution/storage/src/event.rs
@@ -1,3 +1,9 @@
-pub fn test() {
-    todo!()
+use crate::models::event::*;
+use crate::schema::events;
+use diesel::RunQueryDsl;
+use crate::error::StorageError;
+use crate::connections::db_connection::PgPooledConnection;
+
+pub fn get_events(connection: &mut PgPooledConnection) -> Result<Vec<Event>, StorageError> {
+    events::table.load::<Event>(connection).map_err(StorageError::from)
 }
