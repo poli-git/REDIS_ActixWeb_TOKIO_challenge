867e943c06d3dfaf397015f395f36105a8ba7e67
Pablo Bisceglia <86027276+pb-forte@users.noreply.github.com>
2025-06-06 00:07:40 -0300

    Update plan.rs

--- solution/storage/src/plan.rs
+++ solution/storage/src/plan.rs
@@ -1,7 +1,7 @@
 use crate::connections::db::PgPooledConnection;
 use crate::error::StorageError;
 use crate::models::plans::{NewPlan, Plan};
-use crate::schema::plans;
+use crate::schema::plans::{self, plan_end_date, plan_start_date};
 use diesel::insert_into;
 use diesel::prelude::*;
 
@@ -17,6 +17,16 @@ pub fn add_plan(
 ) -> Result<Plan, StorageError> {
     insert_into(plans::table)
         .values(&new_plan)
+        .on_conflict((plans::base_plans_id, plans::event_plan_id))
+        .do_update()
+        .set((
+            plan_start_date.eq(&new_plan.plan_start_date),
+            plan_end_date.eq(&new_plan.plan_end_date),
+            plans::sell_from.eq(&new_plan.sell_from),
+            plans::sell_to.eq(&new_plan.sell_to),
+            plans::sold_out.eq(&new_plan.sold_out),
+            plans::updated_at.eq(diesel::dsl::now), // Use current time for updated_at
+        )) // Handle conflict if the event already exists
         .get_result(connection)
         .map_err(StorageError::from)
 }
