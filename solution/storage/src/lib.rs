befabf2a8f7f4c29ce392c13b41d8cd71836c830
Pablo Bisceglia <86027276+pb-forte@users.noreply.github.com>
2025-06-11 11:11:11 -0300

    Update lib.rs

--- solution/storage/src/lib.rs
+++ solution/storage/src/lib.rs
@@ -7,16 +7,13 @@ extern crate diesel;
 extern crate chrono;
 extern crate dotenv;
 extern crate rand;
-
 extern crate uuid;
 
 pub mod schema;
-
 pub mod base_plan;
 pub mod connections;
 pub mod error;
 pub mod models;
 pub mod plan;
 pub mod zone;
-
 pub mod provider;
